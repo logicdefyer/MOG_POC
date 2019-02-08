0a1bfa7b9f72edabffcaa68b427cbcb0ed779df2
tristup <tristup@itobuz.com>
2019-02-08 19:34:54 +0530

    tabbed_navigation side_navigation

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, Image, View, TextInput } from "react-native";
-import { Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text,Image, StyleSheet, Button, TextInput } from 'react-native';
+import Icon from '@expo/vector-icons/Ionicons';
 import * as data from './userjson.json';
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
-// import {HomeScreen} from './screens/HomeScreen';
+/**
+ * - AppSwitchNavigator
+ *    - WelcomeScreen
+ *      - Login Button
+ *      - Sign Up Button
+ *    - AppDrawerNavigator
+ *          - Dashboard - DashboardStackNavigator(needed for header and to change the header based on the                     tab)
+ *            - DashboardTabNavigator
+ *              - Tab 1 - FeedStack
+ *              - Tab 2 - ProfileStack
+ *              - Tab 3 - SettingsStack
+ *            - Any files you don't want to be a part of the Tab Navigator can go here.
+ */
 
+import {
+  createSwitchNavigator,
+  createAppContainer,
+  createDrawerNavigator,
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+export default App;
 
-class LoginScreen extends React.Component {
+class WelcomeScreen extends Component {
   state = {
     username: "",
     password: "",
@@ -24,8 +48,6 @@ class LoginScreen extends React.Component {
       password: pass
     });
   };
-
-
   placeSubmitHandler = () => {
     if (this.state.username.trim() === "" || this.state.password.trim() === "") {
       alert("Cant be Blank");
@@ -35,21 +57,16 @@ class LoginScreen extends React.Component {
     this.setState(prevState => {
       if(prevState.username===data.userName && prevState.password===data.passWord) {
         alert("Hi "+prevState.username);
-        this.props.navigation.dispatch(StackActions.reset({
-          index: 0,
-          actions: [
-            NavigationActions.navigate({ routeName: 'Home' })
-          ],
-        }))
+        this.props.navigation.navigate('Dashboard')
       } else {
         alert("Login Failed");
       }
     });
   };
-
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
         <View style={styles.inner_container}>
           <TextInput
             placeholder="Username"
@@ -72,56 +89,167 @@ class LoginScreen extends React.Component {
           />
         </View>
       </View>
+      </View>
     );
   }
 }
 
 
-class HomeScreen extends React.Component {
+class DashboardScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>DashboardScreen</Text>
+      </View>
+    );
+  }
+}
+
+class Feed extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Go To Detail Screen" onPress={() => this.props.navigation.navigate('Detail')} />
+      </View>
+    );
+  }
+}
+
+class Settings extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+            title="Log Out"
+            onPress={() => {
+              this.props.navigation.navigate('Welcome')}
+            }
+          />
+      </View>
+    );
+  }
+}
+
+class Profile extends Component {
   state = {
     image: require('./assets/image.jpg')
   };
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>
-        </Text>
         <Image
           source={this.state.image}
         />
-        <Button
-            title="Log Out"
-            onPress={() => {
-              this.props.navigation.dispatch(StackActions.reset({
-                index: 0,
-                actions: [
-                  NavigationActions.navigate({ routeName: 'Login' })
-                ],
-              }))
-            }}
-          />
+        <Text>Profile</Text>
       </View>
     );
-  }  
+  }
 }
 
-const AppNavigator = createStackNavigator({
-  Login: {
-    screen: LoginScreen,
+const Detail = props => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Detail</Text>
+  </View>
+);
+
+const FeedStack = createStackNavigator(
+  {
+    Feed: {
+      screen: Feed,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: 'Feed',
+          headerLeft: (
+            <Icon style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} name="md-menu" size={30} />
+          )
+        };
+      }
+    },
+    Detail: {
+      screen: Detail
+    }
   },
-  Home: {
-    screen: HomeScreen,
-  },
-}, {
-    initialRouteName: 'Login',
+  {
+    defaultNavigationOptions: {
+      gesturesEnabled: false
+    }
+  }
+);
+
+const ProfileStack = createStackNavigator({
+  Profile: {
+    screen: Profile,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: 'Profile',
+        headerLeft: (
+          <Icon style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} name="md-menu" size={30} />
+        )
+      };
+    }
+  }
+});
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: 'Settings',
+        headerLeft: (
+          <Icon style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} name="md-menu" size={30} />
+        )
+      };
+    }
+  }
 });
 
-export default createAppContainer(AppNavigator);
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    FeedStack,
+    ProfileStack,
+    SettingsStack
+  },
+  {
+    navigationOptions: ({ navigation }) => {
+      const { routeName } = navigation.state.routes[navigation.state.index];
+      return {
+        header: null,
+        headerTitle: routeName
+      };
+    }
+  }
+);
+const DashboardStackNavigator = createStackNavigator(
+  {
+    DashboardTabNavigator: DashboardTabNavigator
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => {
+      return {
+        headerLeft: (
+          <Icon style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} name="md-menu" size={30} />
+        )
+      };
+    }
+  }
+);
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Dashboard: {
+    screen: DashboardStackNavigator
+  }
+});
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: { screen: WelcomeScreen },
+  Dashboard: { screen: AppDrawerNavigator }
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center"
   },
