f66ab98a96bb0439befa92aa6b7caabaf600f53f
upayan4u <tristup@itobuz.com>
2019-02-18 17:22:46 +0530

    screen 2 flatlist demo

--- src/screens/screen2.js
+++ src/screens/screen2.js
@@ -1,27 +1,86 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {   StyleSheet,
+    View,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    TouchableOpacity } from 'react-native';
 
+
+import ajax from '../service/FetchData';
 // create a component
 class screen2  extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+          loading: true,
+          dataSource:[]
+         };
+       }
+
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        //fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then((responseJson)=> {
+            this.setState({
+            loading: false,
+            dataSource: responseJson
+            })
+        })
+        .catch(error=>alert(error)) //to catch the errors if any
+        }
+
+    FlatListItemSeparator = () => {
         return (
-            <View style={styles.container}>
-                <Text> screen2 </Text>
-            </View>
+            <View style={{
+                height: 1,
+                width:"100%",
+                backgroundColor:"rgba(0,0,0,0.5)",
+        }}
+        />
         );
+        }
+    renderItem=(data)=>
+    <TouchableOpacity style={styles.list}>
+    <Text style={styles.lightText}>{data.item.name}</Text>
+    <Text style={styles.lightText}>{data.item.email}</Text>
+    <Text style={styles.lightText}>{data.item.company.name}</Text></TouchableOpacity>
+    render(){
+        if(this.state.loading){
+        return( 
+        <View style={styles.loader}> 
+            <ActivityIndicator size="large" color="#0c9"/>
+        </View>
+    )}
+        return(
+            <View style={styles.container}>
+            <FlatList
+            data= {this.state.dataSource}
+            ItemSeparatorComponent = {this.FlatListItemSeparator}
+            renderItem= {item=> this.renderItem(item)}
+            keyExtractor= {item=>item.id.toString()}
+            />
+        </View>
+        )}
     }
-}
 
-// define your styles
-const styles = StyleSheet.create({
-    container: {
+    const styles = StyleSheet.create({
+        container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffffff',
-    },
-});
-
-//make this component available to the app
+        backgroundColor: "#fff"
+        },
+        loader:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+        },
+        list:{
+        paddingVertical: 4,
+        margin: 5,
+        backgroundColor: "#fff"
+        }
+    });
 export default screen2 ;
