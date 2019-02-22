533ac03799bcd28297ee17131d5be7b6cd138ced
upayan4u <tristup@itobuz.com>
2019-02-22 16:26:55 +0530

    native base added

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppContainer from './src/navigation/navigation';
+import Setup from "./src/boot/setup";
 
 class App extends React.Component {
   render() {
-    return <AppContainer />;
+    // return <AppContainer />;
+    return <Setup />;
+
   }
 }
 
