e4dc426d7caf9489c55013124e65df9a2c45a338
upayan4u <tristup@itobuz.com>
2019-02-22 12:36:31 +0530

    new structure with react native cli

---
+++ src/screens/Feed.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+
+import { Colors, Fonts } from "../constants";
+import ImageSlider from "react-native-image-slider";
+import { SearchBar } from "react-native-elements";
+import Slideshow from "react-native-image-slider-show";
+
+const AccessoriesIcon = require("../../assets/pages/Accessories.png");
+const ApparelIcon = require("../../assets/pages/Apparel.png");
+const ElectronicsIcon = require("../../assets/pages/Electronics.png");
+const foodIcon = require("../../assets/pages/food.png");
+const profileIcon = require("../../assets/pages/profile.png");
+const petIcon = require("../../assets/pages/pet.png");
+
+export default class Feed extends Component {
+  _handleResults(results) {
+    this.setState({ results });
+  }
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/* <View style={styles.container}> */}
+        {/* <ImageSlider
+          style={{ height: "20%"}}
+            autoPlayWithInterval={3000}
+            images={[
+              'https://placeimg.com/640/640/nature',
+              'https://placeimg.com/640/640/people',
+              'https://placeimg.com/640/640/animals',
+              'https://placeimg.com/640/640/beer'
+              ]}/> */}
+        <SearchBar placeholder="Search" />
+        <View style={styles.row}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={AccessoriesIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>Accessories</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={foodIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>food</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={petIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>Pet</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={ElectronicsIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>Electronics</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={ApparelIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>Apparel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Detail")}
+            style={styles.item}
+          >
+            <Image
+              resizeMode="contain"
+              source={profileIcon}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <Slideshow
+          dataSource={[
+            { url: "https://placeimg.com/640/640/nature" },
+            { url: "https://placeimg.com/640/640/people" },
+            { url: "https://placeimg.com/640/640/animals" }
+          ]}
+        />
+        {/* </View> */}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    paddingTop: 10
+  },
+  row: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    marginTop: 10
+  },
+  item: {
+    flex: 1,
+    height: 120,
+    paddingVertical: 20,
+    borderColor: Colors.primaryLight,
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginHorizontal: 5
+  },
+  itemText: {
+    color: Colors.primary,
+    fontFamily: Fonts.primary
+  },
+  itemImage: {
+    height: 35
+  }
+});
