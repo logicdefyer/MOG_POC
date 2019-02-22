8cc55331522790fb402a5a808db8907cda09c29e
upayan4u <tristup@itobuz.com>
2019-02-22 12:12:48 +0530

    remove all from upayan

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppContainer from './src/navigation/navigation';
-class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
-}
-export default App;
--- src/constants/Fonts.js
+++
@@ -1,7 +0,0 @@
-// @flow
-export default {
-  primaryLight: 'Lato-Light',
-  primaryRegular: 'Lato-Regular',
-  primaryBold: 'Lato-Bold',
-  primarySemiBold: 'Lato-SemiBold',
-};
